99783e20dbb7edc0309ce353de76ac3385a12333
davr <davr@DESKTOP-0S26F42>
2018-09-09 21:41:29 -0500

    Adelanto clase Conexion en la capa de datos

--- Edu.SenaCsf.LoaniumDesktop.Datos/Conexion.cs
+++ Edu.SenaCsf.LoaniumDesktop.Datos/Conexion.cs
@@ -4,11 +4,31 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Windows.Forms;
+using System.Windows.Forms;
 
 namespace Edu.SenaCsf.LoaniumDesktop.Datos {
     public class Conexion {
-        private static string cadenaConexion = "";
-        private static SqlConnection conexionSql = new SqlConnection(cadenaConexion);
+        private static string cadenaConexion = "Data Source=DESKTOP-0S26F42\\SQLEXPRESS;Initial Catalog=Loanium;Integrated Security=True";
+        private static SqlConnection conn = new SqlConnection(cadenaConexion);
+
+        public static SqlConnection Abrir() {
+            try {
+                conn.Open();
+            } catch (SqlException e) {
+                conn.Close();
+                MessageBox.Show("Error al conectar a la base de datos");
+                
+            }
+
+            return conn;
+        }
+
+        public static void Cerrar() {
+            try {
+                conn.Close();
+            } catch (SqlException e) {
+                MessageBox.Show("No se pudo cerrar la conexión");
+            }
+        }
     }
 }
