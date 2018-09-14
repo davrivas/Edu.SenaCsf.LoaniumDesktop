406e00b8fd9f6832d6b0ad70d62a2e2f74ba59e0
davr <davr@DESKTOP-0S26F42>
2018-09-14 09:30:42 -0500

    Adelanto DAO para lo demás

--- Edu.SenaCsf.LoaniumDesktop.Logica/DiscoDAO.cs
+++ Edu.SenaCsf.LoaniumDesktop.Logica/DiscoDAO.cs
@@ -2,6 +2,7 @@
 using Edu.SenaCsf.LoaniumDesktop.Logica.InterfacesDAO;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,51 @@ namespace Edu.SenaCsf.LoaniumDesktop.Logica {
         }
 
         public DiscoDTO BuscarPorId(int id) {
-            throw new NotImplementedException();
+            try {
+                Conexion.Abrir();
+                DiscoDTO disco = new DiscoDTO();
+                TipoMaterialDAO tmDAO = new TipoMaterialDAO();
+                IdiomaDAO iDAO = new IdiomaDAO();
+                string sql = "SELECT * " +
+                    "FROM Material " +
+                    "WHERE MaterialId = " + id;
+                SqlCommand cmd = new SqlCommand(sql, Conexion.Conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read()) {
+                    int iId = Convert.ToInt32(reader["IdiomaId"].ToString().Trim()),
+                        tmId = Convert.ToInt32(reader["TipoMaterialId"].ToString().Trim());
+                    disco.Id = Convert.ToInt32(reader["MaterialId"].ToString().Trim());
+                    disco.Titulo = reader["TituloMaterial"].ToString().Trim();
+                    disco.Autor = reader["AutorMaterial"].ToString().Trim();
+                    disco.FechaPublicacion = Convert.ToDateTime(reader["FechaPublicacion"].ToString().Trim());
+                    disco.Descripcion = reader["DescripcionMaterial"].ToString().Trim();
+                    disco.Idioma = iDAO.BuscarPorId(iId);
+                    disco.TipoMaterial = tmDAO.BuscarPorId(tmId);
+
+                    Conexion.Abrir();
+                    sql = "SELECT * " +
+                        "FROM Disco " +
+                        "WHERE DiscoId = " + id;
+                    cmd = new SqlCommand(sql, Conexion.Conn);
+                    reader = cmd.ExecuteReader();
+                    if (reader.Read()) {
+                        disco.Duracion = reader["DuracionDisco"].ToString().Trim();
+                        return disco;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            } catch (SqlException e) {
+                Console.WriteLine(e.StackTrace);
+                return null;
+            } finally {
+                if (Conexion.Conn != null) {
+                    Conexion.Cerrar();
+                }
+            }
         }
 
         public void Editar(DiscoDTO obj) {
