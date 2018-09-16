2c9613a27bf853d5fcb4a3ed3cbc96aa6973bea8
davr <davr@DESKTOP-0S26F42>
2018-09-15 19:48:10 -0500

    NuevoMaterial (prueba)

---
+++ Edu.SenaCsf.LoaniumDesktop.Presentacion/NuevoMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edu.SenaCsf.LoaniumDesktop.Presentacion {
+    public partial class NuevoMaterial : Form {
+        public NuevoMaterial() {
+            InitializeComponent();
+        }
+    }
+}
